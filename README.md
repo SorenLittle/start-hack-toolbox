a5e9313b908f6f49cf4b5d23942a8a612fb3b668
SorenLittle <sorenlittle.pdx@gmail.com>
2024-03-20 13:09:55 +0100

    💨 Tailwindcss setup

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+## Setup
+
 ### Environment Variables
 
 | Variable | Default | Purpose |
 |----------|---------|---------|
 | MONGO_DB     | start-hack  | Name of the target database in MongoDB |
+
+### F/E Setup
+
+Open css auto-refresh in a parallel terminal
+
+```bash
+cd src/app/frontend/static
+npm run build
+```
---
+++ src/app/frontend/static/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/*.{html}",
+    "../templates/**/*.{html}"
+  ],
+  theme: {
+    debugScreens: {
+      position: ['bottom', 'right'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-debug-screens')
+],
+}
